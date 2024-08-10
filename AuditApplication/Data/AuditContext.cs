4db6cd562d464c73bb81f0cd3ce57b7acab08020
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2024-08-10 13:24:49 +0100

    Added Data folder and AuditContext

---
+++ AuditApplication/Data/AuditContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AuditApplication.Models;
+namespace AuditApplication.Data;
+
+public class AuditContext : DbContext
+{
+    public AuditContext(DbContextOptions<AuditContext> options) : base(options)
+    {
+    }
+
+    public DbSet<AuditTemplate> AuditTemplates { get; set; }
+    public DbSet<Audit> Audits { get; set; }
+    public DbSet<Section> Sections { get; set; }
+    public DbSet<Question> Questions { get; set; }
+    public DbSet<QuestionResponse> QuestionResponses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Additional configurations can be added here, see: https://docs.microsoft.com/en-us/ef/core/modeling/
+    }
+}

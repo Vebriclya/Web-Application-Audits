ec13333c93ba030016d1e45c1fabb97c6d3d9368
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2024-08-11 17:33:33 +0100

    Fixed delete question functionality - The changes did not break things, they made them work - Good for me - Added data-type to createItemHtml

--- AuditApplication/obj/Debug/net8.0/AuditApplication.AssemblyInfo.cs
+++ AuditApplication/obj/Debug/net8.0/AuditApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuditApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e22793b03e26e7112600f6f2a2a8b56a664eb268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18e386cc87b382d43fcf382dcd06677402ee2b5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuditApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuditApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

b052d86cdc31874a588e1df71b6d571ef7d971fa
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2024-08-11 23:31:29 +0100

    Bootstrap adjustments on the main page - Truly, I hate bootstrap. Tailwind was alright but like

--- AuditApplication/obj/Debug/net8.0/AuditApplication.AssemblyInfo.cs
+++ AuditApplication/obj/Debug/net8.0/AuditApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuditApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb950bf6db540852be86d4638e972061c2bac9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e37fac8d06ebf3be5f20d357a46bcc4561bebcff")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuditApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuditApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

8bbcd32a3253655fe25a3fa24ded88dad5365b71
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2024-08-13 00:58:56 +0100

    Tried removing the AntiForgeryToken and it broke - I think we found the piece of spaghetti that stuck - With that in mind, the if now only takes templateId

--- AuditApplication/obj/Debug/net8.0/AuditApplication.AssemblyInfo.cs
+++ AuditApplication/obj/Debug/net8.0/AuditApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuditApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71f62ecadeb93ef5af85a2cb4b4824ae647d9dd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+649f65e22283476093a85b8791f015565e915aa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuditApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuditApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

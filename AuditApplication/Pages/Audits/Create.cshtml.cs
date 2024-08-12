a7c6dd357da0d7cfe4730a51a9663ec4a7212bec
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2024-08-12 23:14:06 +0100

    Rough skeleton of Create.cshtml.cs - OnPostCreateAuditFromTemplateAsync should do what it says on the tin - GenerateAuditHtml is a little less solid, I've not used sb much so this is just a starting layout

--- AuditApplication/Pages/Audits/Create.cshtml.cs
+++ AuditApplication/Pages/Audits/Create.cshtml.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using AuditApplication.Data;
 using AuditApplication.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
+using Microsoft.Extensions.Primitives;
 
 namespace AuditApplication.Pages.Audits
 {
@@ -70,7 +72,58 @@ namespace AuditApplication.Pages.Audits
 
         private string GenerateAuditHtml(Audit audit)
         {
-            return "placeholder";
+            var sb = new StringBuilder();
+            sb.Append("<div class=\"container-fluid\">");
+            
+            // Title
+            sb.Append("<div class=\"row mb-3\">");
+            sb.Append("<div class=\"col-12\">");
+            sb.AppendFormat("<h3 id=\"auditTitle\">{0}</h3>", audit.AuditName);
+            sb.Append("<a href=\"#\" id=\"editAuditName\">Edit</a>");
+            sb.Append("</div></div>");
+            
+            // Create two-columns
+            sb.Append("<div class=\"row\">");
+            
+            // Left column: Section list
+            sb.Append("<div class=\"col-md-4\">");
+            sb.Append("<ul class=\"list-group\" id=\"sectionList\">");
+            foreach (var section in audit.Sections)
+            {
+                sb.AppendFormat("<li class=\"list-group-item\" data-section-id=\"{0}\">{1}</li>", 
+                    section.Id, section.Name);
+            }
+            sb.Append("</ul></div>");
+            
+            // Right column: Section display
+            sb.Append("<div class=\"col-md-8\"><div id=\"sectionDetails\">");
+            var firstSection = audit.Sections.FirstOrDefault();
+            if (firstSection != null)
+            {
+                sb.AppendFormat("<div class=\"section\" data-section-id=\"{0}\">", firstSection.Id);
+                sb.AppendFormat("<h4 class=\"section-name\">{0}</h4>", firstSection.Name);
+                sb.Append("<a href=\"#\" class=\"rename-section\">Rename</a>");
+                sb.Append("<div class=\"questions\">");
+
+                foreach (var question in firstSection.Questions)
+                {
+                    AppendQuestion(sb, question);
+                }
+
+                sb.Append("</div>");
+                sb.Append("<button class=\"btn btn-primary btn-sm mt-3 add-question\">Add Question</button>");
+                sb.Append("</div>");
+            }
+
+            sb.Append("</div></div>");
+            sb.Append("</div></div>");
+            
+            return sb.ToString();
+        }
+
+        private void AppendQuestion(StringBuilder sb, AuditQuestion question)
+        {
+            
         }
 
     }
